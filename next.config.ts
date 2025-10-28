3fd0a5f76dff40f4283efc0dbc4515e1afca970f
alireza <xbarat.team@gmail.com>
2025-10-28 09:50:23 +0330

    diable disableStaticImages

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
   images: {
+    disableStaticImages: true,
     remotePatterns: [
       {
         protocol: "http",
@@ -11,7 +12,6 @@ const nextConfig: NextConfig = {
         pathname: "/uploads/**",
       },
     ],
-    unoptimized: true,
   },
 };
 
