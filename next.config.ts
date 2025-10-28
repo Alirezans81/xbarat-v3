28f69427f7f8996fb6cfc6cf8f9abc29c497bb6a
alireza <xbarat.team@gmail.com>
2025-10-28 11:13:01 +0330

    enable eslint and disable type validation errors

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
+  typescript: {
+    ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
